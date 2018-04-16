0d976b49444e86a77195bd614d472f2f8087cd85
ghost717 <dawid.zawadzki7717@gmail.com>
2018-04-16 23:42:04 +0200

    remove iatacodes api & add airports.json

--- index.php
+++ index.php
@@ -9,6 +9,30 @@
     <script src="main.js"></script>
 </head>
 <body>
-    
+    <?php 
+        /**
+         * Funkcja zwraca wartość odległości 
+         * pomiędzy punktami w metrach
+         *
+         * @param array $aPoint
+         * @param array $bPoint
+         *
+         * @return number
+        */
+        public function getDistanceInMeters(array $aPoint, array $bPoint)
+        {
+            $earthRadius = 6371000;// promień Ziemi w metrach
+            $diffLat = $aPoint['latitude'] - $bPoint['latitude'];
+            $diffLng = $aPoint['longitude'] - $bPoint['longitude'];
+            $a = sin($diffLat / 2) * sin($diffLat / 2) + cos($bPoint['latitude']) * cos($aPoint['latitude']) * sin($diffLng / 2) * sin($diffLng / 2);
+            
+            $b = 2 * asin(sqrt($a));
+            // $b = 2 * atan2( sqrt($a), sqrt(1-$a) );
+            $distance = round(($earthRadius * $b), 2);
+            
+            return $distance;
+            
+        }//end getDistanceInMeters
+    ?>
 </body>
 </html>
